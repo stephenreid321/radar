fa4819a05f5047cfbbf1521750b7d10bd710a98f
Stephen Reid <stephen@stephenreid.net>
2023-02-27 16:07:08 +0000

    .

--- models/link.rb
+++ models/link.rb
@@ -2,13 +2,17 @@ class Link
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  has_many :linkships, dependent: :destroy
+  belongs_to :message
 
   field :url, type: String
   field :data, type: Hash
 
+  validates_presence_of :url, :data
+  validates_uniqueness_of :url, scope: :message_id
+
   def self.admin_fields
     {
+      message_id: :lookup,
       url: :url,
       data: { type: :text_area, disabled: true }
     }
--- models/message.rb
+++ models/message.rb
@@ -2,7 +2,7 @@ class Message
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  has_many :linkships, dependent: :destroy
+  has_many :links, dependent: :destroy
 
   field :discord_id, type: String
   field :data, type: Hash
@@ -23,22 +23,18 @@ class Message
 
   after_create do
     data['embeds'].each do |embed|
-      link = Link.find_or_create_by(url: embed['url'])
-      link.update_attribute(:data, embed)
-      linkships.create(link: link)
+      links.create(url: embed['url'], data: embed)
     end
   end
 
   def self.populate!
     Message.delete_all
     Link.delete_all
-    Linkship.delete_all
     JSON.parse(DISCORD.get("guilds/#{ENV['GUILD_ID']}/threads/active").body)['threads'].each do |thread|
       channel_id = thread['id']
       JSON.parse(DISCORD.get("channels/#{channel_id}/messages").body).each do |message_data|
         Message.create(discord_id: message_data['id'], data: message_data)
       end
     end
-    200
   end
 end
