8392b86a24e1e8e1a64149a0910911640a90d9e5
Stephen Reid <stephen@stephenreid.net>
2023-03-23 18:12:41 +0000

    .

--- app/assets/graph.js
+++ app/assets/graph.js
@@ -109,7 +109,7 @@ function drawNetwork() {
   cy.on('tap', 'node', function () {
     var node = this
     $('.full-screen').fadeOut(function () {
-      if (urlParams.getAll('tags[]').indexOf(this.data('name')) != -1)
+      if (urlParams.getAll('tags[]').indexOf(node.data('name')) != -1)
         window.location.href = `/?${$.param({ channel: urlParams.get('channel'), tags: $.grep(urlParams.getAll('tags[]'), function (value) { return value != node.data('name') }), q: urlParams.get('q') })}`
       else
         window.location.href = `/?${$.param({ channel: urlParams.get('channel'), tags: urlParams.getAll('tags[]').concat([node.data('name')]), q: urlParams.get('q') })}`
